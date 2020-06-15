a04a87ee7e08d91a53fd9782c77bf8f56cd326c8
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-06-15 15:18:39 -0500

    Add Button to styledLib

--- src/components/styledLib.js
+++ src/components/styledLib.js
@@ -1,5 +1,22 @@
 import styled, {css} from "styled-components"
 
+export const Button = styled.button`
+    cursor: pointer;
+    padding: 0.5em 1em;
+    border-radius: 5px;
+    font-weight: bold;
+    font-size: ${props => props.size ? `var(--${props.size})` : '1.3em'};
+    transition: all .2s ease-in-out;
+    background: ${props => props.primary ? 'var(--medium)' : 'var(--white)'};
+    color: ${props => props.primary ? 'var(--white)' : 'var(--medium)'};
+    border: ${props => props.primary ? 'var(--white)' : 'var(--medium)'} 1.5px solid;
+
+    &:hover {
+        transform: scale(1.05);
+        border: transparent 1.5px solid;
+    }
+`
+
 export const Title = styled.h2`
   font-size: 2em;
   text-align: center;
