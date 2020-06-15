490e54fc2d9c3388acf0a89d6171f18a0933aba1
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-06-14 22:59:23 -0500

    Install styled-components. Update G-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Change plugin default options here, e.g.:
+        // ssr: false
+        // displayName: false,
+        // minify: false
+      },
+    }
+    //... other plugins
+  ]
 }
