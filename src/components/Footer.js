0390845e20caa12199dca5a22708e0311fb3e0d0
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-06-15 14:10:33 -0500

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react"
+import {Link} from "gatsby"
+import styled from "styled-components"
+
+const Footer = () => (
+    <FooterWrapper>
+        <FooterNav>
+            <StyledLink to="/" ><li>HOME</li></StyledLink>
+            <StyledLink to="/criminaldefense" ><li>CRIMINAL DEFENSE</li></StyledLink>
+            <StyledLink to="/st-louis-dwi-layer" ><li>DWI DEFENSE</li></StyledLink>
+            <StyledLink to="/st-louis-traffic-lawyer"><li>TRAFFIC LAW</li></StyledLink>
+            <StyledLink to="/st-louis-personal-injury-lawyer"><li>PERSONAL INJURY</li></StyledLink>
+            <StyledLink to="/about"><li>ATTORNEY BIO</li></StyledLink>
+            <StyledLink to="/client-reviews"><li>CLIENT REVIEWS</li></StyledLink>
+            <StyledLink to="/contact"><li>CONTACT US</li></StyledLink>
+        </FooterNav>
+        <Disclaimer>
+            <p>The information on this website is for general information purposes only. Nothing on this site should be taken as legal advice for any individual case or situation. This information is not intended to create, and receipt or viewing does not constitute, an attorney-client relationship.</p>
+        </Disclaimer>            
+    </FooterWrapper>
+)
+
+
+export default Footer
+
+const FooterWrapper = styled.footer`
+    background-color: var(--darkblue);
+    color: var(--white);
+    padding-top: 15px;
+`
+
+const FooterNav = styled.ul`
+    text-align: center;
+    list-style-type: none;
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0;
+    margin-bottom: 10px;
+
+    @media(min-width: 981px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
+`
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: var(--white);
+    grid-column: ${props => props.gridColumn}
+
+    & li {
+        font-weight: 700;
+        margin: auto 2%;
+    }
+`
+
+const Disclaimer = styled.div`
+    width: 80%;
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 2em 0;
+    text-align: center;
+`
+
