d7c886c42fd034fb2d08aa612e6841cce04dbef9
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-06-16 15:55:34 -0500

    Update css

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -14,18 +14,18 @@ export default ContentSection
 const Section = styled.section`
     
     background-color: var(--${props => props.bgSection ? props.bgSection : 'white'});
-    padding: 2em 0;
+    padding: 1em 0;
     width: 100%;
 `
 const ContentWrapper = styled.div`
     width: 100%;
     margin: 0 auto;
-    padding: 1em .5em; 
+    padding: 1em; 
 
     @media(min-width: 768px) {
       width: 80%;
       max-width: 960px;
-      padding: 1.5em 2.5em;
+      padding: 1.5em 1em;
     }
 `
 
